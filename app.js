53528d40ac1c72d02ca96df32b770b4cf7a4b26f
Matizin46 <mattheus.paes.br@gmail.com>
2025-04-08 21:26:30 -0300

    Idiomas, terminar...

---
+++ app.js
@@ -0,0 +1,15 @@
+function fnIdiomaIngles(){
+    document.getElementById("menu1").innerText = "Home"
+    document.getElementById("menu2").innerText = "Properties"
+    document.getElementById("menu3").innerText = "About Us"
+    document.getElementById("menu4").innerText = "Contact"
+    document.getElementById("titulo").innerText = "Find Your Dream Home"
+}   
+
+function fnIdiomaPortugues(){
+    document.getElementById("menu1").innerText = "Inicial"
+    document.getElementById("menu2").innerText = "Imóveis"
+    document.getElementById("menu3").innerText = "Sobre nós"
+    document.getElementById("menu4").innerText = "Contato"
+    document.getElementById("titulo").innerText = "Encontre a casa dos seus sonhos"
+} 
