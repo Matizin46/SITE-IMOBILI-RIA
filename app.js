c2bd724f56400468b5995427ed5ff9f611eb64e4
Matizin46 <mattheus.paes.br@gmail.com>
2025-04-09 17:42:56 -0300

    Tradução Feita

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ function fnIdiomaIngles(){
     document.getElementById("menu3").innerText = "About Us"
     document.getElementById("menu4").innerText = "Contact"
     document.getElementById("titulo").innerText = "Find Your Dream Home"
+    document.getElementById("subtitulo").innerText = "Explore our wide range of properties and find the perfect home for you."
+    document.getElementById("botao1").innerText = "Search Properties"
+    document.getElementById("botao2").innerText = "Learn More"
+    document.getElementById("contato").innerText = "Get in touch with us today!"
+    document.getElementById("entre").innerText = "Contact Us"
+    document.getElementById("fone").innerText = "Phone"
+    document.getElementById("local").innerText = "Office"
+    document.getElementById("assinatura").innerText = "Subscribe to our newsletter for the latest updates on new features and product releases."
+    document.getElementById("cadastro").innerText = "Subscribe"
+    document.getElementById("direitos").innerText = "© 2025 Guto Xavier. All rights reserved."
 }   
 
 function fnIdiomaPortugues(){
@@ -12,4 +22,14 @@ function fnIdiomaPortugues(){
     document.getElementById("menu3").innerText = "Sobre nós"
     document.getElementById("menu4").innerText = "Contato"
     document.getElementById("titulo").innerText = "Encontre a casa dos seus sonhos"
+    document.getElementById("subtitulo").innerText = "Explore nossa ampla variedade de imóveis e encontre a casa perfeita para você."
+    document.getElementById("botao1").innerText = "Pesquise Imóveis"
+    document.getElementById("botao2").innerText = "Saiba Mais"
+    document.getElementById("contato").innerText = "Entre em contato conosco hoje mesmo!"
+    document.getElementById("entre").innerText = "Entre em contato"
+    document.getElementById("fone").innerText = "Telefone"
+    document.getElementById("local").innerText = "Escritório"
+    document.getElementById("cadastro").innerText = "Assine nossa newsletter para receber as últimas atualizações sobre novos recursos e lançamentos de produtos."
+    document.getElementById("cadastro").innerText = "Cadastrar"
+    document,getElementById("direitos").innerText = "© 2025 Guto Xavier. Todos os direitos reservados."
 } 
